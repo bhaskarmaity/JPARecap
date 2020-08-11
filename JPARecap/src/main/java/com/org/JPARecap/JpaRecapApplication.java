5c34898126eb16bd31df9d3c24cebf21b3b2b87f
bhaskarmaity <bhaskarmaity@users.github.com>
2020-08-11 11:19:43 +0530

    First Commit

---
+++ JPARecap/src/main/java/com/org/JPARecap/JpaRecapApplication.java
@@ -0,0 +1,13 @@
+package com.org.JPARecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRecapApplication.class, args);
+	}
+
+}
---
+++ JPARecap/src/test/java/com/org/JPARecap/JpaRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.JPARecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
